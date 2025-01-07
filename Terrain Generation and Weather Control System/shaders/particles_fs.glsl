e80be7cf2f91127c60ca12b85b76ffc5a7559b47
Re <ww1228747899@outlook.com>
2025-01-07 18:41:12 +0000

    Terrain Generation and Weather Control System

--- Terrain Generation and Weather Control System/shaders/particles_fs.glsl
+++
@@ -1,9 +0,0 @@
-#version 330 core
-
-out vec4 FragColor;
-
-uniform vec3 particleColor; // 动态传递粒子颜色
-
-void main() {
-    FragColor = vec4(particleColor, 1.0); // 使用传递的颜色
-}
--- Terrain Generation and Weather Control System/shaders/snow.fs
+++
@@ -1,9 +0,0 @@
-#version 330 core
-out vec4 FragColor;
-
-uniform vec3 particleColor; // 粒子颜色
-
-void main() {
-    // 设置为粒子的颜色（白色）
-    FragColor = vec4(particleColor, 1.0); // 雪花颜色为不透明的白色
-}
